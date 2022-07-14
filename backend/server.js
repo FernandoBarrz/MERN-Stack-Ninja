dd852947c84471af6ae063a85442246e64cb9c97
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-07-13 21:30:47 -0500

    Added Shema and Models for a workout element

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose')
 
 
 // express app
@@ -18,10 +19,17 @@ app.use((req, res, next)=>{
 // Routes
 app.use("/api/workouts", workoutRoutes)
 
-// Listen for request
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port, ", process.env.PORT);
-})
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI).then(() => {
+
+    // Listen for request
+    app.listen(process.env.PORT, () => {
+        console.log("Listening on port, ", process.env.PORT);
+    })
+
+}).catch((err)=>{
+    console.log(err);
+})    
 
 
 
